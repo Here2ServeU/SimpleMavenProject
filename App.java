8dcc7e321eb16bba224a905ca9848c84437b4c6c
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-12-18 16:22:31 -0600

    Update App.java

--- App.java
+++ App.java
@@ -2,6 +2,6 @@ package com.t2s;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven!");
+        System.out.println("Hello World from T2S!");
     }
 }
