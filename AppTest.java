0ce7c064fd937f908cdec05b2be31304a1dd6a30
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-12-18 16:19:53 -0600

    Create AppTest.java

---
+++ AppTest.java
@@ -0,0 +1,11 @@
+package com.t2s;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertEquals(1, 1);
+    }
+}
